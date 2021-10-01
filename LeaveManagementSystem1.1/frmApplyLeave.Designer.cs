41fd2bbfee575348fe7ef910ab3d355f4bfa8e9e
pamudunimithika99 <rathnathilakamithika1@gmail.com>
2021-10-01 13:42:11 +0530

    APL

--- LeaveManagementSystem1.1/frmApplyLeave.Designer.cs
+++ LeaveManagementSystem1.1/frmApplyLeave.Designer.cs
@@ -581,13 +581,15 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.button1.Font = new System.Drawing.Font("Arial Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(39, 742);
             this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(154, 40);
             this.button1.TabIndex = 48;
             this.button1.Text = "Save";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // btn_new
