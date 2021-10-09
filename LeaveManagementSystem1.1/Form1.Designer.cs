f8b3a9efec07ee8a27973406369435f15cd6105d
pamudunimithika99 <rathnathilakamithika1@gmail.com>
2021-10-09 19:46:54 +0530

    LG

--- LeaveManagementSystem1.1/Form1.Designer.cs
+++ LeaveManagementSystem1.1/Form1.Designer.cs
@@ -142,7 +142,7 @@
             // 
             // timer1
             // 
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+           
             // 
             // Form1
             // 
